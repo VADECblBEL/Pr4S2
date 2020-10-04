650b4098f685b56e3b16847a049f511c1dee2836
Вадим <vadimmm.m@ya.ru>
2020-10-04 12:27:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace Pr4S2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+            q.Enqueue("First");
+            q.Enqueue("Second");
+            q.Enqueue("Third");
+            q.Enqueue("Fourth");
+
+            while (q.Count > 0)
+            {
+                object obj = q.Dequeue();
+                Console.WriteLine("From queue: {0}", obj);
+            }
+
+            Stack stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+
+            while (stack.Count > 0)
+            {
+                object obj = stack.Pop();
+                Console.WriteLine("From stack: {0}", obj);
+            }
+        }
+    }
+}
